e050807cea599d537c0357f1c1e16c7282273d00
Stefan Schmidhuber <stefan.schmidhuber@gmx.at>
2018-03-27 09:25:45 +0200

    generic submit function

--- web/ui.js
+++ web/ui.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
 	var player = null;
 
+	// submit ajax requests
+	submitRequest = function(event, errorFun, successFun) {
+		$.ajax({
+			type: $(event.target).attr("method"),
+			url: $(event.target).attr("action"),
+			data: $(event.target).serialize(),
+			error: errorFun(event),			
+			success: successFun(event)
+		});
+	}
+
 	$(".nav-link").click(function () {
 		var item = $(this).attr("href").substr(1).toLowerCase();
-		//var item = $(this).text().toLowerCase();
 		$(".nav-link").removeClass("active");
 		$(this).addClass("active");
 
@@ -24,34 +34,6 @@ $(document).ready(function () {
 
 	$("#form-register").submit(function (event) {
 		event.preventDefault();
-
-		console.log("submit");
-		console.log($(this).attr("action"));
-		console.log($(this).attr("method"));
-		console.log($("#register-name").val());
-
-		var player = $("#register-name").val();
-		var cou = $("#register-country").val();
-		var pw = $("#register-password").val();
-
-		console.log("name:", player, "\ncountry:", cou, "\npassword:", pw);
-
-		// Send the data using post
-		var posting = $.post("http://127.0.0.1:8000/signUp",
-		{
-			name: player,
-			country: cou,
-			password: pw
-		},
-	);
-		
-		console.log(posting);
-	
-		/*$.post("/signUp", $("form-register").serialize(),
-		function(data) {
-			console.log(data);			
-		  },
-		   'json'
-		);*/
+		submitRequest(event);
 	});
 });
